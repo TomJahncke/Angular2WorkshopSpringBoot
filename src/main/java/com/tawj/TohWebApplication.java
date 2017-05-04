cca384916ed5d97cb2929a8bf4c497bdf679bbcc
Tom Jahncke <TomJahncke@gmail.com>
2017-05-04 08:44:39 -0400

    Built from https://start.spring.io with Web, H2, Actuator, Actuator Docs

---
+++ src/main/java/com/tawj/TohWebApplication.java
@@ -0,0 +1,12 @@
+package com.tawj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TohWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TohWebApplication.class, args);
+	}
+}
