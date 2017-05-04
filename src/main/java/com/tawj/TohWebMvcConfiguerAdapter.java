ff0ae648e79a9281b9eaf7d69ffd4726f8667dce
Tom Jahncke <TomJahncke@gmail.com>
2017-05-04 09:52:29 -0400

    Added logic to server the static content up from an exploded location on local disk instead of jar content when provided at run-time.

--- src/main/java/com/tawj/TohWebMvcConfiguerAdapter.java
+++ src/main/java/com/tawj/TohWebMvcConfiguerAdapter.java
@@ -1,11 +1,38 @@
 package com.tawj;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 public class TohWebMvcConfiguerAdapter extends WebMvcConfigurerAdapter{
+
+    private final String EXPLODED_PATH_KEY="exploded.path";
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+        if (!registry.hasMappingForPattern("/wmsWeb/**")) {
+            ResourceHandlerRegistration tohWebUiResourceHandler = registry.addResourceHandler("/tohWebUi/**");
+
+            String explodedPath = System.getProperty(EXPLODED_PATH_KEY);
+            if (explodedPath != null && !explodedPath.isEmpty()) {
+                tohWebUiResourceHandler.addResourceLocations(explodedPath);
+                tohWebUiResourceHandler.setCacheControl(CacheControl.noCache());
+            } else {
+                tohWebUiResourceHandler.addResourceLocations(
+                        "classpath:/META-INF/resources/tohWebUi/");
+                tohWebUiResourceHandler.setCacheControl(CacheControl.maxAge(1, TimeUnit.DAYS));
+            }
+
+
+        }
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
